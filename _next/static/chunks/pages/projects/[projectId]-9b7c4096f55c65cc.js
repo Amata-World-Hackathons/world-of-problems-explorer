c07343236e29f7e8174da24d8dceb38c4d598a76
yggie <yggie@users.noreply.github.com>
2022-03-14 11:55:23 +0000

    Deploying to gh-pages from @ amata-world/world-of-problems-explorer@99d67d99aa9cf43f7cf6c663a5a5fbd7ce257fcd 🚀

--- _next/static/chunks/pages/projects/[projectId]-9b7c4096f55c65cc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[153],{486:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/projects/[projectId]",function(){return t(6314)}])},6314:function(n,u,t){"use strict";t.r(u);var _=t(5880);u.default=_.default}},function(n){n.O(0,[634,662,508,880,774,888,179],(function(){return u=486,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
